ed277f251951889a747b18d9a79fa79bc118aefa
Leslie Garcia <ggarciaveka@gmail.com>
2026-07-10 15:28:39 -0500

    Resume title ft

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,9 +1,14 @@
+import ResumeTitle from "../../components/resumePage/resumeTitle/ResumeTitle"
 import "./resume.css"
 
 export default function Resume (){
     return (
         <div className="resume">
-            resume
+            <div className="container">
+                <div className="page">
+                    <ResumeTitle/>
+                </div>
+            </div>
         </div>
     )
 }
